e92617650d9024d3019af0dc0b1c3da6bac5dce2
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-08 15:45:38 +0100

    Completed 8 ball functionality

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var ballImageView: UIImageView!
 
+    func randomiseBallImage() {
+        let randomImageName = "ball" + String(Int.random(in: 1...5))
+        
+        ballImageView.image = UIImage(imageLiteralResourceName: randomImageName)
+    }
 
-
+    @IBAction func goButtonTapped(_ sender: UIButton) {
+        randomiseBallImage()
+    }
 }
 
